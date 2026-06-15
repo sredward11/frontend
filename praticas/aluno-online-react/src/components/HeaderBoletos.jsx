43cd10ec7c0d4aa003a2a72aa2cf5ed09bba0b8b
unknown <2424290032@ACADEMICO.IESB.BR>
2026-06-15 09:33:48 -0300

    feat: conclui pratica08. Fecha #13

--- praticas/aluno-online-react/src/components/HeaderBoletos.jsx
+++
@@ -1,8 +0,0 @@
-export default function HeaderBoletos() {
-  return (
-    <header className="flex justify-between items-center mb-10">
-      <h1 className="text-[2rem] font-bold text-[#202020]">Meus Boletos</h1>
-      <img src="/avatar.svg" alt="Avatar do usuário" className="w-10 h-10 rounded-full object-cover" />
-    </header>
-  );
-}
--- praticas/aluno-online-react/src/components/TextoDescritivoDashboard.jsx
+++
@@ -1,7 +0,0 @@
-export default function TextoDescritivoDashboard(){
-    return(
-         <h2 className="text-[1.3rem] font-bold text-[#303030] mb-6">
-            Bem-vindo ao portal do aluno
-          </h2>
-    );
-}
